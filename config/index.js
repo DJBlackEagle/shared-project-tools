0e9c40038460f9e5166ba3b74a848fc45112e653
DJBlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2023-03-09 00:40:08 +0100

    feat(Prettier): add Prettier base config
    Adding Prettier base config for repositories.
    
    ref #5

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 const commitlint = require("./commitlint");
+const prettier = require("./prettier");
 
 module.exports = {
   commitlint,
+  prettier,
 };
---
+++ config/prettier/config-base.js
@@ -0,0 +1,11 @@
+const configPrettier = {
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+};
+
+module.exports = configPrettier;
