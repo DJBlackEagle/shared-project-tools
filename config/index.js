99611239389e5627386b2745837593528c03cbd3
DJBlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2023-03-09 10:35:10 +0100

    feat(release-it): add base config
    Add release-it base config.
    
    ref #13

--- config/index.js
+++ config/index.js
@@ -2,10 +2,12 @@ const commitlint = require('./commitlint');
 const eslint = require('./eslint');
 const nanoStaged = require('./nano-staged');
 const prettier = require('./prettier');
+const releaseIt = require('./release-it');
 
 module.exports = {
   commitlint,
   eslint,
   nanoStaged,
   prettier,
+  releaseIt,
 };
