491cbd0b1017f505f5edea4ef2a9a434ef0f491d
DJBlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2023-06-22 22:43:41 +0200

    feat(nano-staged): add yml extension for Prettier check up
    Add yml extension for Prettier check up.
    
    fix #229

--- config/nano-staged/config-base.js
+++ config/nano-staged/config-base.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,md,html,css,json}': (api) => `prettier --check ${api.filenames.join(' ')}`,
+  '*.{js,jsx,ts,tsx,md,html,css,json,yml}': (api) => `prettier --check ${api.filenames.join(' ')}`,
   '*.{js,jsx,ts,tsx}': (api) => `eslint ${api.filenames.join(' ')}`,
 };
