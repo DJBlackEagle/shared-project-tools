00703a4be7f260fcf53b146c62daa928b1ba9aa7
DJBlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2023-06-28 23:02:49 +0200

    chore(project): add linting on release and remove draft option of GitHub release

--- .release-it.js
+++ .release-it.js
@@ -2,12 +2,8 @@ const config = require('./config');
 
 module.exports = {
   ...config.releaseIt.base,
-  github: {
-    ...config.releaseIt.base.github,
-    draft: true,
-  },
   hooks: {
     ...config.releaseIt.base.hooks,
-    'before:release': 'npx prettier --write . && git add . --update',
+    'before:release': 'yarn run prettier:fix && yarn run lint && git add . --update',
   },
 };
